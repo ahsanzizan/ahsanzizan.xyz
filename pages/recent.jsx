496076cc891096f8abe7630fc71eb6dff67d1995
Ahsan <ahsanaz461@gmail.com>
2023-05-12 17:24:01 +0700

    fix 'recent' page error

--- pages/recent.jsx
+++ pages/recent.jsx
@@ -2,6 +2,7 @@ import Blogs from "@/components/Blogs";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
+import clientProm from "@/lib/mongodb";
 import { useEffect, useState } from "react";
 
 export default function RecentlyUploaded({ data }) {
@@ -13,7 +14,7 @@ export default function RecentlyUploaded({ data }) {
             <Header title={'ahsanAazizan | Recently Uploaded'} description={"Personal Blog"} />
             <Navbar />
             <h1 className="text-secondary text-3xl text-center pt-12">
-                Popular Blogs
+                Recent Blogs
             </h1>
             <Blogs data={blogs} />
             <Footer />
@@ -23,12 +24,12 @@ export default function RecentlyUploaded({ data }) {
 
 export async function getServerSideProps() {
     const connectDB = await clientProm;
-  var getBlogs = await connectDB.db('personal-blog').collection('blog-post').find({}).toArray();
-  var recentlyUploaded = getBlogs.sort((a, b) => b.publishDate - a.publishDate).slice(0, 10);
+    var getBlogs = await connectDB.db('personal-blog').collection('blog-post').find({}).toArray();
+    var recentlyUploaded = getBlogs.sort((a, b) => b.publishDate - a.publishDate).slice(0, 10);
 
-  return {
-      props: {
-          data: JSON.parse(JSON.stringify(recentlyUploaded)).filter(blog => !blog.link.includes('private')),
-      }
-  }
+    return {
+        props: {
+            data: JSON.parse(JSON.stringify(recentlyUploaded)).filter(blog => !blog.link.includes('private')),
+        }
+    }
 }
