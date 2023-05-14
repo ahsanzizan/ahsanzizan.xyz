59d5c2bc299ddfe6695b87a99f1e386a0f93b4a6
Ahsan <ahsanaz461@gmail.com>
2023-05-14 17:49:24 +0700

    add hover effect to blog preview

--- components/BlogPreview.jsx
+++ components/BlogPreview.jsx
@@ -1,44 +1,45 @@
+import Link from "next/link";
 import ActiveLink from "./ActiveLink";
 import { stringifyDate } from "@/lib/stringifyDate";
 
 
 export default function BlogPreview({ publishDate, tags, title, link, previewText, views }) {
   return (
-        <li className="py-5 px-5 md:px-5 my-10 border border-secondary border-opacity-30 rounded-2xl">
-                <article className="w-full">
-                    <div className="space-y-2 md:grid md:grid-cols-4 md:items-baseline md:space-y-0 gap-20">
-                        <div className="mb-5 border-r h-full">
-                            <dl>
-                                <dt className="text-secondary">Published at</dt>
-                                <dd className="text-base font-medium leading-5 text-gray-400">
-                                    <time>{stringifyDate(publishDate)}</time>
-                                </dd>
-                            </dl>
-                            <p className="text-secondary">{views} views</p>
-                        </div>
-                        <div className="space-y-5 md:col-span-3">
-                            <div className="space-y-6">
-                                <div>
-                                    <ActiveLink className="text-2xl font-bold leading-8 tracking-tight text-main hover:underline" href={"/blog/" + link}>
-                                        {title}
-                                    </ActiveLink>
-                                    <div className="flex flex-wrap my-2">
-                                        {tags.map(tag => {
-                                            return <ActiveLink className="rounded-full border-2 border-white px-2 text-secondary font-semibold mr-3 uppercase duration-300 hover:text-cyan-600 hover:bg-secondary" key={tag} href={"/tags/" + tag}>{tag}</ActiveLink>
-                                        })}
+        <li className="my-10 border border-secondary border-opacity-30 rounded-2xl hover:border-opacity-100 hover:shadow-xl transition duration-300">
+            <article className="w-full py-5 px-5">
+                        <div className="space-y-2 md:grid md:grid-cols-4 md:items-baseline md:space-y-0 gap-20">
+                            <div className="mb-5 border-r h-full">
+                                <dl>
+                                    <dt className="text-secondary">Published at</dt>
+                                    <dd className="text-base font-medium leading-5 text-gray-400">
+                                        <time>{stringifyDate(publishDate)}</time>
+                                    </dd>
+                                </dl>
+                                <p className="text-secondary">{views} views</p>
+                            </div>
+                            <div className="space-y-5 md:col-span-3">
+                                <div className="space-y-6">
+                                    <div>
+                                        <ActiveLink className="text-2xl font-bold leading-8 tracking-tight text-main hover:underline" href={"/blog/" + link}>
+                                            {title}
+                                        </ActiveLink>
+                                        <div className="flex flex-wrap my-2">
+                                            {tags.map(tag => {
+                                                return <ActiveLink className="rounded-full border-2 border-white px-2 text-secondary font-semibold mr-3 uppercase duration-300 hover:text-cyan-600 hover:bg-secondary" key={tag} href={"/tags/" + tag}>{tag}</ActiveLink>
+                                            })}
+                                        </div>
+                                    </div>
+                                    <div className="text-ellipsis max-w-none text-gray-300" dangerouslySetInnerHTML={{ __html: normalizePreviewText(previewText) }}>
                                     </div>
                                 </div>
-                                <div className="text-ellipsis max-w-none text-gray-300" dangerouslySetInnerHTML={{ __html: normalizePreviewText(previewText) }}>
+                                <div className="text-base font-medium leading-6">
+                                    <ActiveLink className="transition duration-300 text-main" href={"/blog/" + link}>
+                                        Read more
+                                    </ActiveLink>
                                 </div>
                             </div>
-                            <div className="text-base font-medium leading-6">
-                                <ActiveLink className="transition duration-300 text-main" href={"/blog/" + link}>
-                                    Read more
-                                </ActiveLink>
-                            </div>
                         </div>
-                    </div>
-                </article>
+            </article>
         </li>
     )
 }
