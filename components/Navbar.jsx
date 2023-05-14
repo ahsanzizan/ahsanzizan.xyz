2dc22f0e1595c23a4896ac714035e8e94b17c644
Ahsan <ahsanaz461@gmail.com>
2023-05-14 21:45:48 +0700

    change nav logo font-family

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -22,7 +22,7 @@ export default function Navbar() {
             <header style={{ zIndex: 999, boxShadow: "0 1px 4px rgba(146, 161, 176, 0.15)" }} className="bg-[#222831] sticky top-0 z-50 w-screen mx-auto px-4">
                 <nav className="flex flex-wrap py-1 justify-between">
                     <div className="py-2 mr-5">
-                        <ActiveLink className="text-main text-base md:text-xl no-underline font-sans font-semibold" href="/">ahsanAazizan</ActiveLink>
+                        <ActiveLink className="text-main text-base md:text-xl no-underline font-semibold" href="/">ahsanAazizan</ActiveLink>
                     </div>
                     <div onClick={() => toggleNav(!navShow)} className="block md:hidden" style={{ cursor: "pointer" }}>
                         <a
