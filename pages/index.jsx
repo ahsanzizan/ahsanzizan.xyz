7b139fad96f9ab912a28266e0c43aa4c0dbade09
Ahsan <ahsanaz461@gmail.com>
2023-05-15 22:10:34 +0700

    small changes on homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,7 +17,7 @@ export default function HomePage({ data }) {
       <Navbar />
       <div className="text-secondary px-5 lg:px-48 pt-12">
         <h1 className="text-2xl md:text-3xl font-bold leading-relaxed mb-3"><span className="text-main hover:underline underline-offset-4"><a href="https://ahsanAazizan.github.io/">ahsanAazizan</a></span> - Blog</h1>
-        <p className="text-base md:text-lg"><a className="text-main hover:underline font-semibold">@ahsan</a> made this website because he wanted to share his thought on things to other people, and he thought a blog would fit perfectly.</p>
+        <p className="text-base md:text-lg"><a className="text-main hover:underline font-semibold" href="https://instagram.com/ahsanzizan">@ahsanzizan</a> made this website because he wanted to share his thought on things to other people, and he thought a blog would fit perfectly.</p>
       </div>
       <Search articles={data} setResults={setSearchRes} results={searchRes} />
       <div className="mt-20">
