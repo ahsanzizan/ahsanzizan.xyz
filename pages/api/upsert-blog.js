86d5170f9bc0b58b3fa5ec75516b356fce39306e
Ahsan <ahsanaz461@gmail.com>
2023-05-22 22:41:34 +0700

    small changes

--- pages/api/upsert-blog.js
+++ pages/api/upsert-blog.js
@@ -4,16 +4,17 @@ import { ObjectId } from "mongodb";
 
 export default withSessionRoute(async function uploadHandler(req, res) {
     if (!req.session?.state?.loggedIn) return res.status(401).json({ status: 401, error: "Unauthorized admin" });
+    if (req.method !== 'POST') return res.status(405).json({ status: 403, error: `Method '${req.method}' Not Allowed` }); 
     try {
-        if (req.method !== 'POST') return res.status(405).json({ status: 403, error: `Method '${req.method}' Not Allowed` }); 
         const connectDB = await clientProm;
         const id = req.body._id ? new ObjectId(req.body._id) : new ObjectId(32);
         const isNew = !req.body._id;
         delete req.body._id;
-        const insert = await connectDB.db('personal-blog').collection('blog-post').updateOne({ _id: id }, 
-            { $set: !isNew ? req.body : { ...req.body, clicks: 0, publishDate: new Date().getTime() } }, 
+        const doc = !isNew? req.body : { ...req.body, clicks: 0, publishDate: new Date().getTime() };
+        const upsert = await connectDB.db('personal-blog').collection('blog-post').updateOne({ _id: id }, 
+            { $set: doc }, 
             { upsert: true });
-        return res.send({ status: 200, mongo: insert, isNew: isNew });
+        return res.send({ status: 200, mongo: upsert, isNew: isNew });
     } catch (e) {
         console.log(e);
         res.status(500).json({ status: 500, error: `Internal server error` });
