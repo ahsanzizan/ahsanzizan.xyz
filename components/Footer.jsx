e6a5a5b81e5ced57eaa629e72da5b279c714cfa5
Ahsan <ahsanaz461@gmail.com>
2023-06-02 19:54:35 +0700

    small changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,20 +1,16 @@
 
 export default function Footer() {
     return (
-        <div className="bg-secondary mx-auto w-full py-10 mt-72 text-[#222831] text-center px-3">
-            <div className="block text-center mb-8">
-                <h1 className="font-semibold text-3xl mb-5">Based In</h1>
-                <p className="font-semibold text-xs">Rempoa, East Ciputatat, South Tangerang City, Banten, Indonesia</p>
+        <footer className="footer">
+            <p className="footer__title">Based In</p>
+            <p className="footer__subtitle">Rempoa, East Ciputat, South Tangerang City, Banten, Indonesia.</p>
+            <div className="footer__social">
+                <a href="https://www.linkedin.com/in/ahsan-azizan-33908b250/" className="footer__icon"><i className='bx bxl-linkedin'></i></a>
+                <a href="https://www.instagram.com/ahsanaazizan" className="footer__icon"><i className='bx bxl-instagram'></i></a>
+                <a href="https://github.com/ahsanAazizan" className="footer__icon"><i className='bx bxl-github' ></i></a>
+                <a href="https://www.twitter.com/ahsanaz461" className="footer__icon"><i className='bx bxl-twitter' ></i></a>
             </div>
-            <div className="flex justify-center gap-9 mb-10">
-                <a href="https://www.linkedin.com/in/ahsan-azizan-33908b250/" className="text-[1.5em]"><i className='bx bxl-linkedin'></i></a>
-                <a href="https://www.instagram.com/ahsanzizan" className="text-[1.5em]"><i className='bx bxl-instagram'></i></a>
-                <a href="https://github.com/ahsanzizan" className="text-[1.5em]"><i className='bx bxl-github' ></i></a>
-                <a href="https://www.twitter.com/ahsanaz461" className="text-[1.5em]"><i className='bx bxl-twitter' ></i></a>
-            </div>
-            <h3 className="font-semibold text-sm">
-                © 2023 Ahsan Azizan
-            </h3>
-        </div>
+            <p className="footer__copy">&copy; 2023 Ahsan Azizan</p>
+        </footer>
     )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,7 @@ import React from "react";
 import ContactForm from "@/components/ContactForm";
 import Image from "next/image";
 import Link from "next/link";
+import Footer from "@/components/Footer";
 
 export default class Home extends React.Component {
 
@@ -97,17 +98,7 @@ export default class Home extends React.Component {
                     <ContactForm />
                 </main>
                 
-                <footer className="footer">
-                    <p className="footer__title">Based In</p>
-                    <p className="footer__subtitle">Rempoa, East Ciputat, South Tangerang City, Banten, Indonesia.</p>
-                    <div className="footer__social">
-                        <a href="https://www.linkedin.com/in/ahsan-azizan-33908b250/" className="footer__icon"><i className='bx bxl-linkedin'></i></a>
-                        <a href="https://www.instagram.com/ahsanaazizan" className="footer__icon"><i className='bx bxl-instagram'></i></a>
-                        <a href="https://github.com/ahsanAazizan" className="footer__icon"><i className='bx bxl-github' ></i></a>
-                        <a href="https://www.twitter.com/ahsanaz461" className="footer__icon"><i className='bx bxl-twitter' ></i></a>
-                    </div>
-                    <p className="footer__copy">&copy; 2023 Ahsan Azizan</p>
-                </footer>
+                <Footer />
             </>
         )
     }
