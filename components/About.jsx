f5dba906fe6e1fa711efb88dc1d3f3a6e0f477a4
Ahsan <ahsanaz461@gmail.com>
2023-06-03 20:39:06 +0700

    add padding to about

--- components/About.jsx
+++ components/About.jsx
@@ -10,10 +10,9 @@ export default function About() {
                         <Image src="/about.png" alt="" width={300} height={300} unoptimized />
                     </div>
                     
-                    <div className="text-secondary max-w-lg text-center md:text-left">
+                    <div className="text-secondary max-w-lg text-center md:text-left px-5">
                         <h2 className="font-bold text-2xl mb-3">{"I'm"} <span className="text-main">Ahsan Awadullah Azizan</span></h2>
-                        <p className="text-base leading-5">My name is Ahsan Awadullah Azizan, people often call me Ahsan. {"I'm"} currently living in Malang, but i originally live in Tangerang Selatan. {"I'm"} a student at SMK Telkom Malang, majoring in Software Engineering. My hobbies are coding, gaming, and football.</p>           
-
+                        <p className="text-base leading-5">My name is Ahsan Awadullah Azizan, people often call me Ahsan. {"I'm"} currently living in Malang, but i originally live in Tangerang Selatan. {"I'm"} a student at SMK Telkom Malang, majoring in Software Engineering. My hobbies are coding, gaming, and football.</p>
                     </div>                                   
                 </div>
         </section>
