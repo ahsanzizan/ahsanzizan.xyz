89b9faa6cbbea3e713fda21ff1040ee8282e5797
Ahsan <ahsanaz461@gmail.com>
2023-06-14 19:39:14 +0700

    fix:oversized blog's heading text

--- components/Blog/BlogPage.jsx
+++ components/Blog/BlogPage.jsx
@@ -10,7 +10,7 @@ export default function BlogPage({ blogs, title }) {
         <>
             <Search articles={blogs} results={searchRes} setResults={setSearchRes} />
             <main className="px-5 md:px-24 py-32 transition duration-500 ease-in-out max-w-[1980px] mx-auto">
-                <h1 className="heading-text text-5xl pb-20">{title}</h1>
+                <h1 className="heading-text text-3xl md:text-5xl pb-20">{title}</h1>
                 <Blogs blogs={blogs} />
             </main>
         </>
