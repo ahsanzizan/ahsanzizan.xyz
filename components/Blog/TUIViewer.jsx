8b8c317c37a4027024b7a5636bf5fb052ba8499a
Ahsan <ahsanaz461@gmail.com>
2023-07-02 10:49:01 +0700

    style: change syntax highlighting theme

--- components/Blog/TUIViewer.jsx
+++ components/Blog/TUIViewer.jsx
@@ -1,8 +1,7 @@
 import '@toast-ui/editor/dist/toastui-editor.css';
-import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
 import { Viewer } from '@toast-ui/react-editor';
 import React from 'react';
-import 'prismjs/themes/prism-coy.css';
+import 'prism-themes/themes/prism-one-light.css';
 import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js';
 
 export default React.forwardRef(function WysiwygViewer({ content, viewerRef }) {
