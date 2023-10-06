f09cf4579b609513afcc4ff1b183e0194750f195
Ahsan <ahsanaz461@gmail.com>
2023-10-06 15:04:21 +0700

    feat: add blog api route

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -1,1 +1,17 @@
-    
+import { getAllBlogs } from "@/lib/queries/blog.query";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  try {
+    const getBlogs = await getAllBlogs();
+    return NextResponse.json(
+      { status: 200, message: "Successfully get all blogs", blogs: getBlogs },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { status: 500, message: "Internal Server Error", error },
+      { status: 500 },
+    );
+  }
+}
