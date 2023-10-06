9f136cfa33f2e44e45af6247bd493edf94d34506
Ahsan <ahsanaz461@gmail.com>
2023-10-06 22:47:03 +0700

    feat: finish blog api route

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -1,11 +1,57 @@
-import { getAllBlogs } from "@/lib/queries/blog.query";
+import { createBlog, getAllBlogs } from "@/lib/queries/blog.query";
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+
+type Blog = {
+  title: string;
+  content: string;
+  link: string;
+  tags: string[];
+};
 
 export async function GET(req: Request) {
   try {
-    const getBlogs = await getAllBlogs();
+    const getAll = await getAllBlogs();
     return NextResponse.json(
-      { status: 200, message: "Successfully get all blogs", blogs: getBlogs },
+      {
+        status: 200,
+        message: "Successfully created a new blog",
+        blogs: getAll,
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { status: 500, message: "Internal Server Error", error },
+      { status: 500 },
+    );
+  }
+}
+
+export async function POST(req: Request) {
+  const session = await getServerSession(authOptions);
+  if (!session?.user)
+    return NextResponse.json(
+      { status: 503, message: "Forbidden request" },
+      { status: 503 },
+    );
+
+  try {
+    const { title, content, link, tags }: Blog = await req.json();
+    if (!title || !content || !link)
+      return NextResponse.json(
+        { status: 400, message: "Bad request" },
+        { status: 400 },
+      );
+
+    const newBlog = await createBlog({ title, content, link, tags });
+    return NextResponse.json(
+      {
+        status: 200,
+        message: "Successfully created a new blog",
+        blog: newBlog,
+      },
       { status: 200 },
     );
   } catch (error) {
