30ab5cb6814ab2c0710fcf0ea942ffec05cde952
Ahsan <ahsanaz461@gmail.com>
2023-11-12 10:29:08 +0700

    deps refactor: install next-seo and remove IBlog interface

--- src/lib/queries/blog.query.ts
+++ src/lib/queries/blog.query.ts
@@ -1,20 +1,13 @@
-import BlogModel, { IBlog } from "@/models/Blog.model";
+import BlogModel, { Blog } from "@/models/Blog.model";
 import connectDB from "../mongoose";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth";
 
-export async function getAllBlogs(): Promise<IBlog[] | undefined> {
+export async function getAllBlogs(): Promise<Blog[] | undefined> {
   await connectDB();
   const result = await BlogModel.find({});
   return result;
 }
-
-type Blog = {
-  title: string;
-  content: string;
-  link: string;
-  tags: string[];
-};
 
 export async function createBlog(blog: Blog) {
   await connectDB();
