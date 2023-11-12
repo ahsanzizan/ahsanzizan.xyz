da8c657bdf49ed33fbf27c13365f4b0a0d54be59
Ahsan <ahsanaz461@gmail.com>
2023-11-12 10:42:54 +0700

    chore fix: fix dependencies conflict and uninstall novel, add utilityFunctions

---
+++ src/utils/utilityFunctions.ts
@@ -0,0 +1,4 @@
+export function stringifyDate(date: Date) {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    
+}
