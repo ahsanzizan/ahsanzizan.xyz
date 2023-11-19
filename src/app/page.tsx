a9eeb9d237d444b53b0972f9ddbfe13cc7eb3211
Ahsan <ahsanaz461@gmail.com>
2023-11-19 20:38:02 +0700

    remaking the frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
-import About from "./components/About";
-import Blog from "./components/Blog";
-import Footer from "./components/Footer";
-import Landing from "./components/Landing";
-import Navbar from "./components/Navbar";
-import Works from "./components/Works";
-
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <main className="w-full">
-        <Landing />
-        <About />
-        <Works />
-        <Blog />
-        <Footer />
-      </main>
+        
     </>
-  );
+  )
 }
