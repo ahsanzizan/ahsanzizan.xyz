c792cb490fb334a74f0a864c82584f0adc6e83a2
Ahsan <ahsanaz461@gmail.com>
2023-11-22 22:27:08 +0700

    chore: Add drive.google.com as verified images hostname

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "drive.google.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
