57bbe91097b4c1050b3d4db7d36848a59cf9c746
Ahsan <ahsanaz461@gmail.com>
2023-11-24 18:39:37 +0700

    feat: Responsive navbar and navigation menu

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -22,21 +22,21 @@ export default function Navbar() {
             width={64}
             height={64}
             alt="Logo"
-            className="rounded-full hover:drop-shadow-glow"
+            className="h-8 w-8 rounded-full hover:drop-shadow-glow md:h-16 md:w-16"
           />
         </Link>
         <div className="flex items-center gap-[18px]">
           <a
             href="mailto:ahsanaz461@gmail.com"
-            className="group flex items-center gap-2 rounded-full border border-white px-[22px] py-[10px] text-lg transition-all duration-500 hover:bg-white hover:text-black hover:drop-shadow-glow"
+            className="group flex items-center gap-1 rounded-full border border-white px-4 py-2 text-sm transition-all duration-500 hover:bg-white hover:text-black hover:drop-shadow-glow md:px-[22px] md:py-[10px] md:text-lg"
           >
-            {"Let's Talk"}
+            {"Let's Talk "}
             <svg
               width="16"
               height="16"
               viewBox="0 0 16 16"
               xmlns="http://www.w3.org/2000/svg"
-              className="m-1 fill-current transition-transform duration-500 group-hover:translate-x-1"
+              className="m-1 h-3 w-3 fill-current transition-transform duration-500 group-hover:translate-x-1 md:h-4 md:w-4"
             >
               <path
                 d="M1 8.99998H12.17L7.29 13.88C6.9 14.27 6.9 14.91 7.29 15.3C7.68 15.69 8.31 15.69 8.7 15.3L15.29 8.70998C15.3827 8.61747 15.4563 8.50758 15.5064 8.3866C15.5566 8.26563 15.5824 8.13595 15.5824 8.00498C15.5824 7.87401 15.5566 7.74433 15.5064 7.62336C15.4563 7.50238 15.3827 7.39249 15.29 7.29998L8.71 0.699979C8.61742 0.607397 8.50751 0.533957 8.38654 0.483852C8.26558 0.433747 8.13593 0.407959 8.005 0.407959C7.87407 0.407959 7.74442 0.433747 7.62346 0.483852C7.50249 0.533957 7.39258 0.607397 7.3 0.699979C7.20742 0.792561 7.13398 0.902472 7.08387 1.02344C7.03377 1.1444 7.00798 1.27405 7.00798 1.40498C7.00798 1.53591 7.03377 1.66556 7.08387 1.78652C7.13398 1.90749 7.20742 2.0174 7.3 2.10998L12.17 6.99998H1C0.45 6.99998 0 7.44998 0 7.99998C0 8.54998 0.45 8.99998 1 8.99998Z"
@@ -48,14 +48,14 @@ export default function Navbar() {
             onClick={() => {
               setNavOpen(true);
             }}
-            className="rounded-full border border-white px-[22px] py-4 transition-all duration-500 hover:bg-white hover:text-black hover:drop-shadow-glow"
+            className="rounded-full border border-white px-4 py-[14px] transition-all duration-500 hover:bg-white hover:text-black hover:drop-shadow-glow md:px-[22px] md:py-4"
           >
             <svg
               width="20"
               height="12"
               viewBox="0 0 20 12"
               xmlns="http://www.w3.org/2000/svg"
-              className="stroke-current"
+              className="h-2 w-4 stroke-current md:h-3 md:w-5"
             >
               <path
                 d="M1 11H19M1 6H19M1 1H19"
@@ -84,21 +84,21 @@ export default function Navbar() {
                 width={64}
                 height={64}
                 alt="Logo"
-                className="rounded-full"
+                className="h-8 w-8 rounded-full hover:drop-shadow-glow md:h-16 md:w-16"
               />
             </Link>
             <button
               onClick={() => {
                 setNavOpen(false);
               }}
-              className="rounded-full border border-white px-[22px] py-4 transition-all duration-500 hover:bg-white hover:text-black hover:drop-shadow-glow"
+              className="rounded-full border border-white px-4 py-[14px] transition-all duration-500 hover:bg-white hover:text-black hover:drop-shadow-glow md:px-[22px] md:py-4"
             >
               <svg
                 width="20"
                 height="12"
                 viewBox="0 0 20 12"
                 xmlns="http://www.w3.org/2000/svg"
-                className="stroke-current"
+                className="h-2 w-4 stroke-current md:h-3 md:w-5"
               >
                 <path
                   d="M1 0.999756L19 10.9998M1 11L19 0.999756"
@@ -115,7 +115,7 @@ export default function Navbar() {
               <Link
                 key={i}
                 href={link.href}
-                className={`text-7xl transition-all duration-500 after:h-[2px] hover:drop-shadow-glow`}
+                className={`text-5xl transition-all duration-500 hover:drop-shadow-glow md:text-7xl`}
                 style={{
                   color: `${
                     activeLinkIdx !== null
