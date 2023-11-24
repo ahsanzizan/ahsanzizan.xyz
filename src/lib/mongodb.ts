027397e844ba5db326caacedcc26d2e7c9b99a6b
Ahsan <ahsanaz461@gmail.com>
2023-11-25 05:44:02 +0700

    chore: Remove mongodb utility file

--- src/lib/mongodb.ts
+++
@@ -1,7 +0,0 @@
-import { MongoClient } from "mongodb";
-
-const MONGO_URI: string = process.env.MONGO_URI;
-const client: MongoClient = new MongoClient(MONGO_URI);
-const clientProm = client.connect();
-
-export default clientProm;
