73f7a3942b69b43d41f55681a61e9cd6c7bf9437
Ahsan <ahsanaz461@gmail.com>
2023-12-01 18:42:03 +0700

    feat: Add description field to project

--- src/models/Project.model.ts
+++ src/models/Project.model.ts
@@ -2,6 +2,7 @@ import { InferSchemaType, Schema, model, models } from "mongoose";
 
 const ProjectSchema = new Schema({
   title: { type: String, required: true },
+  description: { type: String, required: true },
   url: { type: String, required: true },
   image: { type: String, required: true },
   link: { type: String, required: true },
