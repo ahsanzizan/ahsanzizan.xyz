0fce844e4a11c50751fb676573477e164e3b5fd7
Ahsan <ahsanaz461@gmail.com>
2023-12-01 16:36:01 +0700

    feat: Complete header in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,15 @@
 import Navbar from "../components/Parts/Navbar";
 import Wrapper from "../components/Wrapper";
+import Footer from "../components/Parts/Footer";
+import Header from "./components/Header";
 
 export default function About() {
   return (
     <Wrapper>
       <Navbar />
       <main className="mx-auto w-full max-w-[1440px] px-5 py-[137px]">
-        
+        <Header />
+        <Footer />
       </main>
     </Wrapper>
   );
