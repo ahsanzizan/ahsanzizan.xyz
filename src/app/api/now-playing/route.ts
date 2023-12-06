8bab062dffa5b23707c8e680b8e787abb4a8351c
Ahsan <ahsanaz461@gmail.com>
2023-12-06 19:43:14 +0700

    fix: Apply try catch block to prevent unwanted exception

--- src/app/api/now-playing/route.ts
+++ src/app/api/now-playing/route.ts
@@ -2,32 +2,40 @@ import { getNowPlaying } from "@/lib/spotify";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const res = await getNowPlaying();
+  try {
+    const res = await getNowPlaying();
 
-  if (
-    res.status === 204 ||
-    res.status > 400 ||
-    res.data.currently_playing_type !== "track"
-  ) {
+    if (
+      res.status === 204 ||
+      res.status > 400 ||
+      res.data.currently_playing_type !== "track"
+    ) {
+      return NextResponse.json(
+        { message: "Not playing anything", status: 200, isPlaying: false },
+        { status: 200 },
+      );
+    }
+
+    const data = {
+      isPlaying: res.data.is_playing,
+      title: res.data.item.name,
+      album: res.data.item.album.name,
+      artist: res.data.item.album.artists
+        .map((artist: any) => artist.name)
+        .join(", "),
+      albumImageUrl: res.data.item.album.images[0].url,
+      songUrl: res.data.item.external_urls.spotify,
+    };
+
     return NextResponse.json(
-      { message: "Not playing anything", status: 200, isPlaying: false },
+      { status: 200, message: "success", data },
       { status: 200 },
     );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { status: 500, message: "internal server error" },
+      { status: 500 },
+    );
   }
-
-  const data = {
-    isPlaying: res.data.is_playing,
-    title: res.data.item.name,
-    album: res.data.item.album.name,
-    artist: res.data.item.album.artists
-      .map((artist: any) => artist.name)
-      .join(", "),
-    albumImageUrl: res.data.item.album.images[0].url,
-    songUrl: res.data.item.external_urls.spotify,
-  };
-
-  return NextResponse.json(
-    { status: 200, message: "success", data },
-    { status: 200 },
-  );
 }
