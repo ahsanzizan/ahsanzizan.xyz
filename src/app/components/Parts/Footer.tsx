09392fbf3342ba5678f0a4d5d5d47b16bd7ee9a0
Ahsan <ahsanaz461@gmail.com>
2023-12-18 13:57:26 +0700

    fix: Redesign footer

--- src/app/components/Parts/Footer.tsx
+++ src/app/components/Parts/Footer.tsx
@@ -1,8 +1,7 @@
 import { getContentbyKey } from "@/database/content.query";
 import { getAllSocialMedias } from "@/database/socialMedia.query";
-import { StandardLinkButton } from "../global/Buttons";
-import LeftArrowIcon from "../global/Icons/LeftArrow";
 import SocialMediasRow from "../global/SocialMediasRow";
+import Link from "next/link";
 
 export default async function Footer() {
   const socialMedias = await getAllSocialMedias();
@@ -11,14 +10,14 @@ export default async function Footer() {
   return (
     <>
       <footer className="flex w-full flex-col items-start gap-4 md:flex-row md:justify-between">
-        <div className="block">
-          <StandardLinkButton
-            href={"mailto:" + email?.content || "ahsanaz461@gmail.com"}
-          >
-            {"Let's Talk "}
-            <LeftArrowIcon className="m-1 h-3 w-3 fill-current transition-transform duration-500 group-hover:translate-x-1 md:h-4 md:w-4" />
-          </StandardLinkButton>
-        </div>
+        <Link
+          href={"mailto:" + email?.content || "ahsanaz461@gmail.com"}
+          className="transition-all duration-500 hover:ml-2"
+        >
+          <h3 className="mb-[14px] text-base drop-shadow-glow sm:text-2xl lg:text-[28px]">
+            {email?.content || "ahsanaz461@gmail.com"}
+          </h3>
+        </Link>
         <div>
           <SocialMediasRow socialMedias={socialMedias} />
         </div>
