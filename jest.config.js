a2a579c522a07ad237828066138c5fd7cbfbe1de
Ahsan <ahsanaz461@gmail.com>
2023-12-18 15:20:44 +0700

    chore(test): Resetup jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-  },
-  moduleNameMapper: {
-    "^.+\\.(css|less|scss)$": "identity-obj-proxy",
-  },
-};
