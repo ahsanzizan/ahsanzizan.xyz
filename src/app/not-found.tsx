6bdcbe72e442808be82a500c38f2bdc7fba04684
Ahsan <ahsanaz461@gmail.com>
2023-12-20 12:27:51 +0700

    feat: Add not found error boundary page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,25 @@
+import { StandardLinkButton } from "./components/global/Buttons";
+import LeftArrowIcon from "./components/global/Icons/LeftArrow";
+
+export default function NotFound() {
+  return (
+    <>
+      <section className="flex min-h-screen w-screen items-center justify-center">
+        <header className="flex flex-col items-center justify-center gap-2 md:gap-4">
+          <h1 className="text-center text-6xl leading-snug drop-shadow-glow md:text-9xl">
+            404
+          </h1>
+          <h5 className="text-md mb-8 text-center md:text-xl">
+            The page you are looking for is not available.
+          </h5>
+          <div className="block">
+            <StandardLinkButton href="https://www.ahsanzizan.xyz">
+              To My Website
+              <LeftArrowIcon className="ml-1 fill-current transition-transform duration-500 group-hover:translate-x-1" />
+            </StandardLinkButton>
+          </div>
+        </header>
+      </section>
+    </>
+  );
+}
