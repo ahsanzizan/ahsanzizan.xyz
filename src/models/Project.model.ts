01db3f2ec69f917c49fc2ef071775a0fe14089cd
Ahsan <ahsanaz461@gmail.com>
2023-12-22 20:30:02 +0700

    feat: Add isWorkedOn attr to project model

--- src/models/Project.model.ts
+++ src/models/Project.model.ts
@@ -7,6 +7,7 @@ export const ProjectSchema = new Schema({
   url: { type: String, required: true },
   image: { type: String, required: true },
   link: { type: String, required: true, unique: true },
+  currentlyWorkingOn: { type: Boolean },
 });
 
 export default models.Project<Project> ||
