71a0deff8f1c9cf1c1831cb0624156771257d038
Ahsan <ahsanaz461@gmail.com>
2024-01-02 05:58:39 +0700

    feat: Remove ArrowIcon from 'lets talk' button

--- src/app/components/Parts/Navbar.tsx
+++ src/app/components/Parts/Navbar.tsx
@@ -36,7 +36,6 @@ export default function Navbar({ email }: { email: string }) {
         <div className="flex items-center gap-[18px]">
           <StandardLinkButton href={"mailto:" + email}>
             {"Let's Talk "}
-            <LeftArrowIcon className="m-1 h-3 w-3 fill-current transition-transform duration-500 group-hover:translate-x-1 md:h-4 md:w-4" />
           </StandardLinkButton>
           <button
             onClick={() => {
