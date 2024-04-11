a7c45253f62eabfd553316061889c5250887b3a7
Ahsan <ahsanaz461@gmail.com>
2024-04-11 07:31:10 +0700

    feat(ui): Add new navigation bar

---
+++ src/app/components/global/ui/navigation-bar.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import cn from "@/lib/clsx";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "/about", text: "About" },
+  { href: "/works", text: "Works" },
+  { href: "/blog", text: "Blog" },
+];
+
+export default function NavigationBar() {
+  const pathname = usePathname();
+
+  return (
+    <nav className={cn("fixed top-12 flex w-full justify-center px-5")}>
+      <ul
+        className={cn(
+          "flex w-full max-w-sm items-center justify-between rounded-full border border-neutral-500 bg-black p-2",
+        )}
+      >
+        {links.map((link) => {
+          const currentRoute = pathname.split("/")[1];
+          const isActive =
+            currentRoute === link.href.toLowerCase().split("/")[1];
+
+          return (
+            <li
+              key={link.text}
+              className={cn(
+                `rounded-full transition-all duration-300 hover:bg-neutral-600 ${
+                  isActive ? "bg-neutral-800 hover:bg-neutral-800" : "bg-black"
+                }`,
+              )}
+            >
+              <Link href={link.href} className={cn("block px-5 py-2")}>
+                <span className={cn("text-sm md:text-base")}>{link.text}</span>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
--- src/app/ui-tes/page.tsx
+++ src/app/ui-tes/page.tsx
@@ -1,12 +1,22 @@
-import { Button, Link } from "../components/global/ui/Button";
+import { Button, Link } from "../components/global/ui/button";
+import NavigationBar from "../components/global/ui/navigation-bar";
 
 export default function TesUI() {
   return (
-    <div className="flex h-screen w-full items-center justify-center gap-11">
-      <Link href="#" variant={"default"}>
-        Link
-      </Link>
-      <Button variant={"inverse"}>Button</Button>
-    </div>
+    <>
+      <NavigationBar />
+      <div className="flex h-screen w-full items-center justify-center gap-11">
+        <Link href="#" variant={"default"}>
+          Link
+        </Link>
+        <Button variant={"inverse"}>Button</Button>
+      </div>
+      <div className="flex h-screen w-full items-center justify-center gap-11">
+        <Link href="#" variant={"default"}>
+          Link
+        </Link>
+        <Button variant={"inverse"}>Button</Button>
+      </div>
+    </>
   );
 }
