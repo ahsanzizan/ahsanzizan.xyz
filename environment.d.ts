eed2b1fcc9f4885c6e7624a59ce5a54626583cb6
Ahsan <ahsanaz461@gmail.com>
2024-04-15 06:14:29 +0700

    chore: Migrating to prisma

--- environment.d.ts
+++ environment.d.ts
@@ -1,4 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+
 declare global {
+  var prisma: PrismaClient;
+
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: "development" | "production";
