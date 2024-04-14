5db7fdaf7db491585ab38d28f10bcdeba50fdeb8
Ahsan <ahsanaz461@gmail.com>
2024-04-14 08:52:30 +0700

    fix(ui): Clamp content to 4 lines

--- src/app/components/global/ui/blog-preview.tsx
+++ src/app/components/global/ui/blog-preview.tsx
@@ -1,24 +1,18 @@
 import cn from "@/lib/clsx";
-import { calculateReadTime, truncateString } from "@/utils/utilities";
+import { Blog } from "@/types/models";
+import { calculateReadTime } from "@/utils/utilities";
 import { Anchor } from "./anchor";
 import { H4, P } from "./text";
-import { Blog } from "@/types/models";
 
 interface BlogPreviewProps {
   blog: Blog;
-  contentThreshold?: number;
 }
 
-export function BlogPreview({
-  blog,
-  contentThreshold,
-}: Readonly<BlogPreviewProps>) {
+export function BlogPreview({ blog }: Readonly<BlogPreviewProps>) {
   return (
     <div className={cn("w-full py-10")}>
       <H4 className="mb-[14px] flex items-center gap-1">{blog.title}</H4>
-      <P className="mb-3">
-        {truncateString(blog.content, contentThreshold ?? 300)}
-      </P>
+      <P className="mb-3 line-clamp-4">{blog.content}</P>
       <div className={cn("flex items-center gap-2")}>
         <Anchor href={"/blog/" + blog.link} variant={"default"}>
           Read more
