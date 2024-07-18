7e6620e4006f7bda40df447477503c0acc4abe34
Ahsan Awadullah Azizan <ahsanaz461@gmail.com>
2024-07-18 13:52:25 +0700

    fix(SEO): Use priority for a top-priority routes only in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,19 +8,32 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const blogsRoutes = blogs.map((blog) => ({
     url: `${process.env.NEXTAUTH_URL}/blog/${blog.link}`,
     lastModified: new Date().toISOString(),
-    priority: 0.8,
-    changeFrequency: "monthly",
+    changeFrequency: "yearly",
   }));
   const worksRoutes = works.map((work) => ({
     url: `${process.env.NEXTAUTH_URL}/works/${work.link}`,
     lastModified: new Date().toISOString(),
   }));
-  const conventionalRoutes = ["", "about", "works", "blogs"].map((r) => ({
+  const conventionalRoutes = ["works", "blogs"].map((r) => ({
     url: `${process.env.NEXTAUTH_URL}/${r}`,
     lastModified: new Date().toISOString(),
-    priority: 1,
-    changeFrequency: "yearly",
+    changeFrequency: "monthly",
   }));
 
-  return [...conventionalRoutes, ...blogsRoutes, ...worksRoutes];
+  return [
+    {
+      url: `${process.env.NEXTAUTH_URL}/`,
+      lastModified: new Date().toISOString(),
+      priority: 1,
+      changeFrequency: "never",
+    },
+    {
+      url: `${process.env.NEXTAUTH_URL}/about`,
+      lastModified: new Date().toISOString(),
+      priority: 0.8,
+      changeFrequency: "never",
+    },
+    ...conventionalRoutes,
+    ...blogsRoutes,
+    ...worksRoutes];
 }
